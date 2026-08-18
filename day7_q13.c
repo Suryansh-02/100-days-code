51ae8cd9872e37d2e99c8db96cf4c88050370a96
Suryansh-02 <Suryansh.36976@stu.upes.ac.in>
2026-08-18 19:25:48 +0530

    Add files via upload

---
+++ day7_q13.c
@@ -0,0 +1,19 @@
+/*Q13: Write a program to input a year and check whether it is a leap year or not using conditional statements.*/
+
+
+#include <stdio.h>
+int main()
+{
+    int y;
+    printf("Enter the Year: ");
+    scanf("%d",&y);
+    if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+    {
+        printf("%d is a Leap year.\n",y);
+    }
+    else 
+    {
+        printf("%d is Not a Leap Year.\n",y);
+    }
+    return 0;
+}
